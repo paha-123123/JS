1a5dc9378507fcb179446ef4610abb14e2a97beb
pasha12341234 <127036221+paha-123123@users.noreply.github.com>
2023-06-29 20:54:15 +0200

    1

---
+++ lessons1.js
@@ -0,0 +1,79 @@
+// const tupe = typeof "usuefhusuef"
+// console.log(tupe)
+// узнать тип данных
+
+
+
+// const x = confirm("будет ли подписка")
+// console.log(x)
+
+// const y = prompt("klsjfejio")
+// console.log(y)
+// уточнить в диалоговом окне что то
+
+
+
+// let quantity = prompt("введите количкество товаров")
+// quantity = Number(quantity)
+// console.log(quantity)
+// приведет строку к числу
+
+
+
+// let elementWidth = '50.5px'
+// elementWidth = Number.parseFloat(elementWidth)
+// console.log(elementWidth)
+
+// Выводит в консоль дробное число как в примере 50.5
+
+
+// let elementWidth = '50.5px'
+// elementWidth = Number.parseInt(elementWidth)
+// console.log(elementWidth)
+// console.log(typeof(elementWidth))
+// Выводит в консоль целое число тоесть только 50 из примера
+
+
+
+// let salary = 1300.1642
+// salary = Number(salary.toFixed(1))
+// console.log(salary)
+// console.log(typeof (salary))
+
+// отсеает лишние числа tofixed
+
+
+
+// const base = 2
+// const power = 5
+
+// const result = Math.pow(base, power)
+
+
+// console.log(result)
+
+// приводит число в степень Math.pow(число,степень)
+
+
+
+
+// Пример
+
+// let x = prompt("Введите число")
+// x = Number(x)
+// let y = prompt("Ввведите степень")
+// y = Number(y)
+
+// const result = Math.pow(x, y)
+// console.log(result)
+
+
+
+const max = 50
+const min = 25
+
+const result = Math.round(Math.random() * (max - min) + min)
+
+console.log(result)
+
+// сделал рандомный выбор от 25 до 50 и округлил число
